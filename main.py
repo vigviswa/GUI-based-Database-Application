bddc42414841e91f695ba6a4fc3912a8d6aa99a2
Vignesh Viswanathan <53799125+vigviswa@users.noreply.github.com>
2020-12-13 22:55:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+# Import all required modules
+
 from contacts import app
 from contacts.views import contact, render_template
 
 app.register_blueprint(contact)
 app.url_map.strict_slashes = False
 
+# Error Handling
 
 @app.errorhandler(404)
 def page_not_found(e):
